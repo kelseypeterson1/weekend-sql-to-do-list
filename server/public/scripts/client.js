00108498c49c5e8fa0eb80bc81198b0523943989
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-02-12 16:44:01 -0600

    get function on client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,3 +3,31 @@ $(function() {
 });
 
 
+
+// get tasks from the server
+function getTasks() {
+    $("#tasksTableBody").empty();
+    $.ajax({
+        type: 'GET',
+        url: `/tasks`
+    }).then(function (response) {
+        console.log("GET /tasks response", response);
+        // append data to the DOM
+        // for (let i = 0; i < response.length; i++) {
+        //     $('#songsTableBody').append(`
+        //         <tr data-id=${response[i].id}>
+        //             <td>${response[i].artist}</td>
+        //             <td>${response[i].track}</td>
+        //             <td>${response[i].rank}
+        //                 <button class="btn-vote">UP</button>
+        //                 <button class="btn-vote">DOWN</button>
+        //             </td>
+        //             <td>${response[i].published}</td>
+        //             <td>
+        //                 <button class="btn-delete" data-id=${response[i].id}>Delete</button>
+        //             </td>
+        //         </tr>
+        //     `);
+        // }
+    });
+}
