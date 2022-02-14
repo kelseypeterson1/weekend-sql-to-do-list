323c1d142c1bb467b06eeb13b6ff06bc4900fd02
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-02-14 16:03:55 -0600

    set up pg to work on Heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,7 +1,7 @@
 const pg = require('pg');
 const Pool = pg.Pool;
 const config = {
-  database: 'tasks',
+  database: process.env.DATABASE_NAME || 'tasks',
   host: 'localhost',
   port: 5432,
   max: 10,
--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use('/tasks', router);
 
 
 // Start express
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('up and running on port', PORT);
 });
